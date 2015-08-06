aa55bc27861ad3c089bb9ae2bc2a39e353bf3bb0
Thomas Stewart <thomas.w.stewart@biola.edu>
2015-08-06 15:58:33 -0700

    :)

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 $(function(){ //DOM Ready
+
   
+
   $(".gridster ul").gridster({
       widget_margins: [10, 10],
-      widget_base_dimensions: [140, 140]
+      widget_base_dimensions: [300, 300]
   });
 
 });
