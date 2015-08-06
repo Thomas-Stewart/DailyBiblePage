ffba4a025b10cc8d7bbe68369bbc3fc6036c0fa8
aelazr <azraelbrown@gmail.com>
2015-08-06 22:12:03 +0000

    Adding rss feeds

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $(function(){ //DOM Ready
 
   $(".gridster ul").gridster({
       widget_margins: [10, 10],
-      widget_base_dimensions: [300, 300]
+      widget_base_dimensions: [300, 420]
   });
 
 });
