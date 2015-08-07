551693a66c2c510ebdb9bc5917df056fdd4bf7cb
Thomas Stewart <thomas.w.stewart@biola.edu>
2015-08-06 20:49:34 -0700

    see when draggable

--- main.js
+++ main.js
@@ -2,7 +2,17 @@ $(function(){ //DOM Ready
 
     $(".gridster ul").gridster({
         widget_margins: [10, 10],
-        widget_base_dimensions: [350, 400]
+        widget_base_dimensions: [350, 400],
+        draggable: {
+          start: function(){
+            console.log('starting to drag');
+          },
+          stop: function(){
+            console.log('stopping drag');
+          }
+        }
     });
+
+
 
 });
