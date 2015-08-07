a60d48d37dc2808a12e5d25f9dd993bf11c62415
Thomas Stewart <thomas.w.stewart@biola.edu>
2015-08-06 19:05:59 -0700

    this is a good save point

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(function(){ //DOM Ready
 
     $(".gridster ul").gridster({
         widget_margins: [10, 10],
-        widget_base_dimensions: [350, 450]
+        widget_base_dimensions: [350, 413]
     });
 
 });
